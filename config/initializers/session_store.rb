57660889116845ae78f8a4d7abe1e074f7ddb597
johndewolf <jdewolf06@gmail.com>
2014-02-01 15:02:07 -0500

    initial setup

---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+MentorProgram::Application.config.session_store :cookie_store, key: '_mentor_program_session'
---
+++ spec/features/user_signs_up_spec.rb
@@ -0,0 +1,20 @@
+require 'spec_helper'
+
+feature 'user sign up', %Q{
+  As an unautheticated user
+  I want to sign up
+  So that I can use the service
+} do
+  scenario 'with valid and required attributes' do
+    visit root_path
+    click_link "Sign Up"
+    fill_in 'First name', with: 'test'
+    fill_in 'Last name', with: 'test'
+    fill_in 'Email', with: 'test@aol.com'
+    fill_in 'Password', with: 'password'
+    fill_in 'Password confirmation', with: 'password'
+    select 'Winter 2014', from: 'Cohort'
+    click_button 'Sign up'
+    expect(page).to have_content("Welcome! You have signed up successfully" )
+  end
+end
